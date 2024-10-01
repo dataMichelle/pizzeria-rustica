09faf928e04652bec4db2160b00a4d8d0adf47c6
Michelle <116389047+dataMichelle@users.noreply.github.com>
2024-10-01 13:45:51 -0500

    new route

--- app/api/checkout/route.js
+++ app/api/checkout/route.js
@@ -1,13 +1,12 @@
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-console.log("Stripe Secret Key Loaded:", process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
+  // Only allow POST requests
   if (req.method === "POST") {
     try {
-      const { items } = req.body;
-      console.log("Received items:", items);
+      const { items, totalPrice } = req.body;
 
       // Create a new Checkout Session
       const session = await stripe.checkout.sessions.create({
@@ -27,15 +26,14 @@ export default async function handler(req, res) {
         cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/cancel`,
       });
 
-      console.log("Created Stripe Session:", session);
-
       res.status(200).json({ id: session.id });
     } catch (error) {
       console.error("Stripe session creation error:", error);
       res.status(500).json({ error: error.message });
     }
   } else {
-    res.setHeader("Allow", "POST");
-    res.status(405).end("Method Not Allowed");
+    // If the method is not POST, return a 405 status code
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
