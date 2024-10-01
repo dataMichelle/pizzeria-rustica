1dc426fec79c360baf252426c15ca85fd9715e7c
Michelle <116389047+dataMichelle@users.noreply.github.com>
2024-10-01 09:36:57 -0500

    made cart mobile friendly

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -11,8 +11,10 @@ const handleProceedToCheckout = () => {
 
 export default function CartPage() {
   return (
-    <div>
-      <h1 className="text-3xl text-center my-8">Your Cart</h1>
+    <div className="container mx-auto px-4 py-6 md:px-8 lg:px-12">
+      <h1 className="text-2xl md:text-3xl text-center my-4 md:my-8">
+        Your Cart
+      </h1>
       <Cart />
     </div>
   );
