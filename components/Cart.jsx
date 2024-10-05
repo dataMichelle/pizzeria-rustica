191ad5779be846a099b82b201740711b116df535
Michelle <116389047+dataMichelle@users.noreply.github.com>
2024-10-05 18:38:36 -0500

    fixed padding

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -148,7 +148,7 @@ export default function Cart() {
 
         {/* Proceed to Checkout Button */}
         <button
-          className="bg-green-500 text-white px-4 my-5 rounded"
+          className="bg-green-500 text-white px-4 py-2 my-5 rounded"
           onClick={handleProceedToCheckout}
         >
           Proceed to Checkout
