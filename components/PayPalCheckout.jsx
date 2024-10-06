8e6cf99519420c4a2a3d6c4dbaa38c073f59dfc1
Michelle <116389047+dataMichelle@users.noreply.github.com>
2024-10-06 13:42:58 -0500

    debug

--- components/PayPalCheckout.jsx
+++ components/PayPalCheckout.jsx
@@ -59,6 +59,8 @@ export default function CheckoutPage() {
           {/* Replace form with PayPal buttons */}
           {cartItems.length > 0 && (
             <div className="my-4">
+              console.log("PayPal Client ID:",
+              process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID);
               <PayPalScriptProvider
                 options={{
                   "client-id": process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID,
