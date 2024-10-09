2806f12f022c43cd1c7c6fc85552717af7b8a344
Michelle <116389047+dataMichelle@users.noreply.github.com>
2024-10-09 15:28:11 -0500

    images

--- app/page.js
+++ app/page.js
@@ -26,7 +26,7 @@ export default function HomePage() {
         </div>
 
         {/* Image Section */}
-        <div className="relative w-full h-[40vh] md:h-[25vh] lg:h-[35vh] xl:h-[45] xl:w-[30]vw">
+        <div className="relative w-full h-[40vh] md:h-[25vh] lg:h-[35vh] xl:h-[45vh] xl:w-[30vw]">
           <Image
             src="/hero_serving_pizza.png"
             alt="Pizzeria Rustica Hero Image"
